071697ef3437be700652b15f452d233e233056ea
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-07-20 11:41:47 +0545

    feat(domain): models related to Recommendation added

---
+++ core/domain/src/main/java/com/example/core/domain/model/recommendation/RecommendationConnection.kt
@@ -0,0 +1,5 @@
+package com.example.core.domain.model.recommendation
+
+data class RecommendationConnection(
+    val nodes: List<Recommendation> = listOf()
+)
---
+++ core/domain/src/main/java/com/example/core/domain/model/recommendation/RecommendationRating.kt
@@ -0,0 +1,7 @@
+package com.example.core.domain.model.recommendation
+
+enum class RecommendationRating {
+    NO_RATING,
+    RATE_UP,
+    RATE_DOWN
+}
