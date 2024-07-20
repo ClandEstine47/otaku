33396f1fea67c2689c81abc157f46332cad5a15b
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-07-20 11:48:04 +0545

    feat(domain): models related to User statistic added

---
+++ core/domain/src/main/java/com/example/core/domain/model/user/statistic/UserStaffStatistic.kt
@@ -0,0 +1,12 @@
+package com.example.core.domain.model.user.statistic
+
+import com.example.core.domain.model.staff.Staff
+
+data class UserStaffStatistic(
+    override val count: Int = 0,
+    override val meanScore: Double = 0.0,
+    override val minutesWatched: Int = 0,
+    override val chaptersRead: Int = 0,
+    override val mediaIds: List<Int> = listOf(),
+    val staff: Staff? = null
+): UserStatisticsDetail
---
+++ core/domain/src/main/java/com/example/core/domain/model/user/statistic/UserStudioStatistic.kt
@@ -0,0 +1,12 @@
+package com.example.core.domain.model.user.statistic
+
+import com.example.core.domain.model.studio.Studio
+
+data class UserStudioStatistic(
+    override val count: Int = 0,
+    override val meanScore: Double = 0.0,
+    override val minutesWatched: Int = 0,
+    override val chaptersRead: Int = 0,
+    override val mediaIds: List<Int> = listOf(),
+    val studio: Studio? = null
+): UserStatisticsDetail
