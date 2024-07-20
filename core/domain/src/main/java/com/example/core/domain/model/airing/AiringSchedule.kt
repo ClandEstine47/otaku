5b8790a1379fd82ff3d197d9c8062854020600ea
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-07-20 11:38:58 +0545

    feat(domain): models related to AiringSchedule

---
+++ core/domain/src/main/java/com/example/core/domain/model/airing/AiringSchedule.kt
@@ -0,0 +1,11 @@
+package com.example.core.domain.model.airing
+
+import com.example.core.domain.model.media.Media
+
+data class AiringSchedule(
+    val id: Int = 0,
+    val airingAt: Int = 0,
+    val timeUntilAiring: Int = 0,
+    val episode: Int = 0,
+    val media: Media = Media()
+)
