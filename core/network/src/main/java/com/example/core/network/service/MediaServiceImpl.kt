249d1d9eecd2bc97ef9ebf40a24ad6268a3b85b5
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-07-22 10:02:36 +0545

    feat(network): mappers for media added

---
+++ core/network/src/main/java/com/example/core/network/service/MediaServiceImpl.kt
@@ -0,0 +1,21 @@
+package com.example.core.network.service
+
+import com.apollographql.apollo3.ApolloClient
+import com.example.core.domain.model.airing.AiringSchedule
+import com.example.core.domain.service.MediaService
+import com.example.core.network.RecentlyUpdatedQuery
+
+class MediaServiceImpl(
+    private val apolloClient: ApolloClient
+): MediaService {
+    override suspend fun getTrendingMediaList(pageNumber: Int, airingTimeInMs: Int): List<AiringSchedule> {
+        return apolloClient
+            .query(RecentlyUpdatedQuery(page = pageNumber, airingAtLesser = airingTimeInMs))
+            .execute()
+            .data
+            ?.Page
+            ?.airingSchedules
+            ?.mapNotNull { it?.toRecentlyUpdatedMedia() }
+            ?: emptyList()
+    }
+}
