50c8ccd5e7437ace7f1e0671cb714b7679720471
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-07-25 11:16:30 +0545

    feat(network): fun to fetch trending media added in MediaService

--- core/domain/src/main/java/com/example/core/domain/service/MediaService.kt
+++ core/domain/src/main/java/com/example/core/domain/service/MediaService.kt
@@ -1,8 +1,11 @@
 package com.example.core.domain.service
 
 import com.example.core.domain.model.airing.AiringSchedule
+import com.example.core.domain.model.media.Media
 
 interface MediaService {
 
-    suspend fun getTrendingMediaList(pageNumber: Int, airingTimeInMs: Int): Result<List<AiringSchedule>>
+    suspend fun getRecentlyUpdatedMediaList(pageNumber: Int, airingTimeInMs: Int): Result<List<AiringSchedule>>
+
+    suspend fun getTrendingNowMediaList(pageNumber: Int): Result<List<Media>>
 }
