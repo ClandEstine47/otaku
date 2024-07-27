6abfa6c117402d5cbafe16eae0744772429ff977
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-07-27 20:03:52 +0545

    feat: add kotlin serialization

--- core/navigation/build.gradle.kts
+++ core/navigation/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 apply(from = rootProject.file("spotless.gradle"))
@@ -42,4 +43,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // serialization
+    implementation(libs.kotlinx.serialization.json)
 }
