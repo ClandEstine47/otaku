d41506b13ae75e22f2251317e0331f933dc516d4
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-08-07 09:33:41 +0545

    feat: relocate MainNavigation in app module

--- app/src/main/java/com/example/otaku/OtakuMain.kt
+++ app/src/main/java/com/example/otaku/OtakuMain.kt
@@ -1,8 +1,11 @@
 package com.example.otaku
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.example.core.navigation.MainNavigation
 import com.example.core.navigation.NavActionManager
 import com.example.otaku.ui.theme.OtakuTheme
 
@@ -12,10 +15,15 @@ fun OtakuMain() {
         val navController = rememberNavController()
         val navActionManager = NavActionManager.rememberNavActionManager(navController)
 
-        MainNavigation(
-            navController = navController,
-            navActionManager = navActionManager,
-            deepLink = null,
-        )
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            MainNavigation(
+                navController = navController,
+                navActionManager = navActionManager,
+                deepLink = null,
+            )
+        }
     }
 }
