d76556c84a0a8c226a9b28b1455ced3bc3d4e8c6
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-08-09 21:56:08 +0545

    feat: add OtakuImageCardTitle composable

--- feature/src/main/java/com/example/feature/anime/Common.kt
+++ feature/src/main/java/com/example/feature/anime/Common.kt
@@ -1,11 +1,14 @@
 package com.example.feature.anime
 
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun OtakuTitle(
@@ -17,5 +20,21 @@ fun OtakuTitle(
         modifier = modifier,
         style = MaterialTheme.typography.titleMedium,
         fontWeight = FontWeight.Bold,
+    )
+}
+
+@Composable
+fun OtakuImageCardTitle(
+    modifier: Modifier = Modifier,
+    title: String,
+) {
+    Text(
+        text = title,
+        style = MaterialTheme.typography.bodyMedium,
+        fontWeight = FontWeight.Normal,
+        maxLines = 2,
+        minLines = 2,
+        overflow = TextOverflow.Ellipsis,
+        modifier = modifier.width(100.dp),
     )
 }
