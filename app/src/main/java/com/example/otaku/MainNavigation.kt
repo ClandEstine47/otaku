6f166ded29bec690c31f7308324489ef2775220b
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-08-13 10:50:45 +0545

    feat: add MangaView in MainNavigation

--- app/src/main/java/com/example/otaku/MainNavigation.kt
+++ app/src/main/java/com/example/otaku/MainNavigation.kt
@@ -1,5 +1,6 @@
 package com.example.otaku
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,12 +12,14 @@ import com.example.core.navigation.DeepLink
 import com.example.core.navigation.NavActionManager
 import com.example.core.navigation.OtakuScreen
 import com.example.feature.anime.AnimeView
+import com.example.feature.manga.MangaView
 
 @Composable
 fun MainNavigation(
     navController: NavHostController,
     navActionManager: NavActionManager,
     deepLink: DeepLink?,
+    padding: PaddingValues,
 ) {
     NavHost(
         navController = navController,
@@ -25,6 +28,10 @@ fun MainNavigation(
     ) {
         composable<OtakuScreen.AnimeTab> {
             AnimeView(navActionManager = navActionManager)
+        }
+
+        composable<OtakuScreen.MangaTab> {
+            MangaView(navActionManager = navActionManager)
         }
     }
 }
