b996122c106c64c1b00e059b2f9bf169cb9188fa
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-08-14 21:12:43 +0545

    feat: show trending manga in MangaView

--- feature/src/main/java/com/example/feature/manga/MangaView.kt
+++ feature/src/main/java/com/example/feature/manga/MangaView.kt
@@ -1,17 +1,68 @@
 package com.example.feature.manga
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.core.domain.model.media.Media
 import com.example.core.navigation.NavActionManager
+import com.example.feature.anime.InfiniteHorizontalPager
 
 @Composable
 fun MangaView(
     navActionManager: NavActionManager,
     mangaViewModel: MangaViewModel = hiltViewModel(),
 ) {
-    Column {
-        Text(text = "Manga View")
+    val uiState by mangaViewModel.state.collectAsStateWithLifecycle()
+
+    Column(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .absolutePadding()
+                .verticalScroll(rememberScrollState()),
+    ) {
+        if (uiState.isLoading) {
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxSize()
+                        .padding(top = 60.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                CircularProgressIndicator()
+            }
+        } else {
+            MangaContent(
+                navActionManager = navActionManager,
+                trendingNowMedia = uiState.trendingNowMedia,
+            )
+        }
     }
+}
+
+@Composable
+fun MangaContent(
+    navActionManager: NavActionManager,
+    trendingNowMedia: List<Media>? = null,
+) {
+    if (trendingNowMedia != null) {
+        InfiniteHorizontalPager(mediaList = trendingNowMedia)
+    }
+
+    Spacer(modifier = Modifier.height(40.dp))
 }
--- feature/src/main/java/com/example/feature/manga/MangaViewModel.kt
+++ feature/src/main/java/com/example/feature/manga/MangaViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.feature.manga
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.core.domain.model.media.MediaType
 import com.example.core.domain.repository.MediaRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +17,63 @@ class MangaViewModel
     @Inject
     constructor(
         private val mediaRepository: MediaRepository,
-    ) : ViewModel()
+    ) : ViewModel() {
+        private val mediaType = MediaType.MANGA
+
+        private val _state =
+            MutableStateFlow(
+                MangaUiState(),
+            )
+        val state = _state.asStateFlow()
+
+        init {
+            viewModelScope.launch {
+                loadData()
+            }
+        }
+
+        private suspend fun loadData() {
+            viewModelScope.launch {
+                _state.update {
+                    it.copy(
+                        isLoading = true,
+                    )
+                }
+
+                val trendingNowResultDeferred =
+                    async {
+                        mediaRepository.getTrendingNowMedia(
+                            pageNumber = 1,
+                            perPage = 20,
+                            mediaType = mediaType,
+                        )
+                    }
+
+                val trendingNowResult = trendingNowResultDeferred.await()
+
+                _state.update { currentState ->
+                    when {
+                        trendingNowResult.isSuccess ->
+                            currentState.copy(
+                                trendingNowMedia = trendingNowResult.getOrNull(),
+                                isLoading = false,
+                                error = null,
+                            )
+
+                        trendingNowResult.isFailure ->
+                            currentState.copy(
+                                trendingNowMedia = null,
+                                isLoading = false,
+                                error = trendingNowResult.exceptionOrNull()?.message ?: "An unknown error occurred",
+                            )
+
+                        else ->
+                            currentState.copy(
+                                isLoading = false,
+                                error = "Unexpected result state",
+                            )
+                    }
+                }
+            }
+        }
+    }
