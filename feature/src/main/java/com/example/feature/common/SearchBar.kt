17e2038997085f600c8d70377d2452f34c4c12ee
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-10-08 11:39:26 +0545

    fix: rounded ripple effect on SearchBar

--- feature/src/main/java/com/example/feature/common/SearchBar.kt
+++ feature/src/main/java/com/example/feature/common/SearchBar.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.core.domain.model.media.MediaType
@@ -45,6 +46,7 @@ fun SearchBar(
                     color = MaterialTheme.colorScheme.background.copy(alpha = 0.2f),
                     shape = RoundedCornerShape(50.dp),
                 )
+                .clip(RoundedCornerShape(50.dp))
                 .clickable {
                     onSearchBarClick()
                 },
