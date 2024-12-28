7a5960e5ffca94ebad1cb4f1c5a8e6e22f0ec926
Mausam Saru Magar <mausamsarumagar20@gmail.com>
2024-12-28 22:06:28 +0545

    fix

--- feature/src/main/java/com/example/feature/screens/mediadetail/MediaDetailViewModel.kt
+++ feature/src/main/java/com/example/feature/screens/mediadetail/MediaDetailViewModel.kt
@@ -27,7 +27,7 @@ class MediaDetailViewModel
             getMediaThreads(id)
         }
 
-        private fun getMediaDetail(id: Int) {
+        fun getMediaDetail(id: Int) {
             viewModelScope.launch {
                 _state.update {
                     it.copy(
@@ -66,7 +66,7 @@ class MediaDetailViewModel
             }
         }
 
-        private fun getMediaThreads(
+        fun getMediaThreads(
             mediaId: Int,
         ) {
             viewModelScope.launch {
